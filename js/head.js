edefb34358dc310d835d9f0150c478158e50b462
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2022-06-16 22:53:48 -0300

    adicionando

---
+++ js/head.js
@@ -0,0 +1,16 @@
+var head = "<meta http-equiv = 'Content-Type' content='text/html; charset=UTF-8'>\n\
+            <title>JS Puro</title>\n\
+            <meta http-equiv='cache-control' content='no-cache'>\n\
+            <meta http-equiv='pragma' content='no-cache'>\n\
+            <meta http-equiv='content-language' content='pt-br, en-US, fr'>\n\
+            <meta http-equiv='expires' content = '0'>\n\
+            <meta name='author' content='Bruno Gressler da Silveira'>\n\
+            <meta name='viewport' content='width=device-width, initial-scale=1'>\n\
+            <meta name='description' content='Ao escrever uma meta tag, use entre 140 e 160 caracteres para que o Google possa exibir toda a sua mensagem. Não se esqueça de incluir sua palavra-chave!'>\n\
+            <meta name='copyright' content='© 2021 Bruno Silveira'>\n\
+            <meta name='robots' content='index, follow'>\n\
+            <script src='style/jqueryToast.css'></script>\n\
+            <script src='js/jquery.js'></script>\n\
+            <script src='js/jqueryToast.js'></script>";
+
+document.write(head);
